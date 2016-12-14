cd7a4aac3b8154ac733c63a6ab0818f664edd759
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2016-12-14 09:51:43 -0600

    Improved PropertyChange notification

--- Miruken.Mvc/Controller.cs
+++ Miruken.Mvc/Controller.cs
@@ -6,6 +6,10 @@ using Miruken.Mvc.Views;
 
 namespace Miruken.Mvc
 {
+    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+    using Infrastructure;
+
     public delegate IHandler FilterBuilder(IHandler handler);
 
     public class Controller : Handler,
@@ -120,9 +124,12 @@ namespace Miruken.Mvc
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected virtual void OnPropertyChanged(string propertyName)
+        protected bool ChangeProperty<T>(ref T field, T value,
+               IEqualityComparer<T> comparer = null, 
+               [CallerMemberName] string propertyName = null)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return PropertyChanged.ChangeProperty(
+                ref field, value, this, comparer, propertyName);
         }
 
         #endregion
