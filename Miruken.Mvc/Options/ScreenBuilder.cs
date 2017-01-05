bcbdb760d458224765ac5d726a193fbea8aad8eb
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2017-01-05 07:25:38 -0600

    Add ScreenBuilder for WindowOptions

---
+++ Miruken.Mvc/Options/ScreenBuilder.cs
@@ -0,0 +1,69 @@
+﻿namespace Miruken.Mvc.Options
+{
+    using System.Linq;
+    using System.Windows.Forms;
+
+    public class ScreenBuilder
+    {
+        public ScreenBuilder(WindowOptions options)
+        {
+            WindowOptions = options;
+        }
+
+        public WindowOptions WindowOptions { get; }
+
+        public ScreenBuilder Primary()
+        {
+            WindowOptions.Screen = Screen.PrimaryScreen;
+            return this;
+        }
+
+        public ScreenBuilder Secondary()
+        {
+            WindowOptions.Screen = Screen.AllScreens.FirstOrDefault(s => !s.Primary);
+            return this;
+        }
+
+        public ScreenBuilder FullScreen()
+        {
+            WindowOptions.FillScreen = ScreenFill.FullScreen;
+            return this;
+        }
+
+        public ScreenBuilder VirtualScreen()
+        {
+            WindowOptions.FillScreen = ScreenFill.VirtualScreen;
+            return this;
+        }
+
+        public ScreenBuilder SplitLeft()
+        {
+            WindowOptions.FillScreen = ScreenFill.SplitLeft;
+            return this;
+        }
+
+        public ScreenBuilder SplitRight()
+        {
+            WindowOptions.FillScreen = ScreenFill.SplitRight;
+            return this;
+        }
+
+        public ScreenBuilder SplitTop()
+        {
+            WindowOptions.FillScreen = ScreenFill.SplitTop;
+            return this;
+        }
+
+        public ScreenBuilder SplitBottom()
+        {
+            WindowOptions.FillScreen = ScreenFill.SplitBottom;
+            return this;
+        }
+
+        public ScreenBuilder FillScreen(ScreenFill fillScreen)
+        {
+            WindowOptions.FillScreen = fillScreen;
+            return this;
+        }
+    }
+}
--- Miruken.Mvc/Options/WindowOptions.cs
+++ Miruken.Mvc/Options/WindowOptions.cs
@@ -1,75 +1,89 @@
 ﻿namespace Miruken.Mvc.Options
 {
+    using System;
     using System.Windows.Forms;
     using Callback;
 
+    public enum ScreenFill
+    {
+        FullScreen,
+        VirtualScreen,
+        SplitLeft,
+        SplitRight,
+        SplitTop,
+        SplitBottom
+    }
+
     public class WindowOptions : CallbackOptions<WindowOptions>
     {
-        public bool?  Window          { get; set; }
-        public bool?  Modal           { get; set; }
-        public bool?  PrimaryScreen   { get; set; }
-        public bool?  SecondaryScreen { get; set; }
-        public Screen Screen          { get; set; }
- 
+        public bool?       NewWindow  { get; set; }
+        public bool?       Modal      { get; set; }
+        public Screen      Screen     { get; set; }
+        public ScreenFill? FillScreen { get; set; }
+
         public override void MergeInto(WindowOptions other)
         {
-            if (Window.HasValue && !other.Window.HasValue)
-                other.Window = Window;
+            if (NewWindow.HasValue && !other.NewWindow.HasValue)
+                other.NewWindow = NewWindow;
 
             if (Modal.HasValue && !other.Modal.HasValue)
                 other.Modal = Modal;
 
-            if (PrimaryScreen.HasValue && !other.PrimaryScreen.HasValue)
-                other.PrimaryScreen = PrimaryScreen;
-
-            if (SecondaryScreen.HasValue && !other.SecondaryScreen.HasValue)
-                other.SecondaryScreen = SecondaryScreen;
-
             if (Screen != null && other.Screen == null)
                 other.Screen = Screen;
+
+            if (FillScreen.HasValue && !other.FillScreen.HasValue)
+                other.FillScreen = FillScreen;
         }
     }
 
     public static class WindowOptionsExtensions
     {
-        public static IHandler Window(this IHandler handler)
+        public static IHandler NewWindow(this IHandler handler, Screen screen = null)
         {
             return new RegionOptions
             {
-                Window = new WindowOptions { Window = true }
+                Window = new WindowOptions
+                {
+                    NewWindow = true,
+                    Screen    = screen
+                }
             }.Decorate(handler);
         }
 
-        public static IHandler Modal(this IHandler handler)
+        public static IHandler NewWindow(this IHandler handler, Action<ScreenBuilder> build)
+        {
+            var window = new WindowOptions { NewWindow = true };
+            if (build != null)
+            {
+                var builder = new ScreenBuilder(window);
+                build(builder);
+            }
+            return new RegionOptions {  Window = window }.Decorate(handler);
+        }
+
+        public static IHandler Modal(this IHandler handler, Screen screen = null)
         {
             return new RegionOptions
             {
-                Window = new WindowOptions { Modal = true }
+                Window = new WindowOptions
+                {
+                    NewWindow = true,
+                    Modal     = true,
+                    Screen    = screen
+                }
             }.Decorate(handler);
         }
 
-        public static IHandler PrimaryScreen(this IHandler handler)
+        public static IHandler Modal(this IHandler handler, Action<ScreenBuilder> build)
         {
-            return new RegionOptions
+            var window = new WindowOptions { NewWindow = true, Modal = true };
+            if (build != null)
             {
-                Window = new WindowOptions { PrimaryScreen = true }
-            }.Decorate(handler);
-        }
-
-        public static IHandler SecondaryScreen(this IHandler handler)
-        {
-            return new RegionOptions
-            {
-                Window = new WindowOptions { SecondaryScreen = true }
-            }.Decorate(handler);
-        }
-
-        public static IHandler Screen(this IHandler handler, Screen screen)
-        {
-            return new RegionOptions
-            {
-                Window = new WindowOptions { Screen = screen }
-            }.Decorate(handler);
+                var builder = new ScreenBuilder(window);
+                build(builder);
+            }
+            return new RegionOptions { Window = window }.Decorate(handler);
         }
     }
 }
