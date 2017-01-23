01c3c24a711d40ffa23c28bc53f3aade19b840b4
Craig Neuwirt <craig.neuwirt@improvingenterprises.com>
2017-01-23 11:13:57 -0600

    Added ViewModel

---
+++ Miruken.Mvc/ViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace Miruken.Mvc
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    using Infrastructure;
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool ChangeProperty<T>(ref T field, T value,
+               IEqualityComparer<T> comparer = null,
+               [CallerMemberName] string propertyName = null)
+        {
+            return PropertyChanged.ChangeProperty(
+                ref field, value, this, comparer, propertyName);
+        }
+
+        protected bool ChangeProperty<T>(T property, T value, Action<T> set,
+            IEqualityComparer<T> comparer = null,
+            [CallerMemberName] string propertyName = null)
+        {
+            var ret = ChangeProperty(ref property, value, comparer, propertyName);
+            if (ret) set?.Invoke(property);
+            return ret;
+        }
+    }
+}
